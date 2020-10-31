f7d2b080e925f59a5b175f1d8ed9eff76ebaeb28
Jonathan Rusk <jrusk@chromis.com>
2020-10-31 14:17:12 -0700

    add menu and hours, tweak colors and layout (#1)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+import { Anchor, Box, Grommet, Footer, Text } from "grommet";
+
 import "../styles/globals.css";
 
+const sisterTheme = {
+  global: {
+    colors: {
+      brand: "#393A3A",
+      "neutral-2": "#ED4837"
+    }
+  }
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Grommet theme={sisterTheme}>
+      <Component {...pageProps} />
+
+      <Footer background="brand" pad="medium" fill="horizontal">
+        <Text>&#169; Sister Helen {new Date().getFullYear()}</Text>
+        <Box>1820 W Northern Ave, Suite 140 Phoenix, AZ 85021</Box>
+        <Box>
+          (602) 870-0313
+          <Anchor
+            label="hello@sisterhelenphx.com"
+            href="mailto:hello@sisterhelenphx.com"
+            color="neutral-2"
+          />
+        </Box>
+      </Footer>
+    </Grommet>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,98 @@
 import Head from "next/head";
 import {
   Anchor,
-  Clock,
-  Footer,
-  Grommet,
+  Box,
   Heading,
   Image,
   Main,
-  Text,
-  WorldMap
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Text
 } from "grommet";
 
 export default function Home() {
   return (
-    <Grommet>
+    <div>
       <Head>
         <title>Sister Helen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Main pad="large">
-        <Heading color="neutral-2">Sister Helen</Heading>
-        <Heading level="3" margin="none" color="neutral-2">
-          Coming soon
-        </Heading>
-        <WorldMap
-          color="neutral-1"
+        <Image
           fill="horizontal"
-          places={[
-            {
-              name: "Phoenix",
-              location: [32.46392431882024, -113.74193548387098],
-              color: "accent-2"
-            }
-          ]}
+          fit="contain"
+          src="logo-rect.png"
+          a11yTitle="Sister Helen PHX"
         />
-        <Text color="status-critical">
-          <Clock
-            type="digital"
-            size="xlarge"
-            color="neutral-2"
-            margin="medium"
-            hourLimit={12}
-          />
-        </Text>
-        <Image fill="horizontal" fit="contain" src="/frites.jpg" />
+        <Box direction="row" wrap={true}>
+          <Box pad="small">
+            <Heading level="3">Hours</Heading>
+            <Table alignSelf="start">
+              <TableBody>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Sunday</strong>
+                  </TableCell>
+                  <TableCell>11:00 AM–8:00 PM</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Monday</strong>
+                  </TableCell>
+                  <TableCell>Closed</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Tuesday</strong>
+                  </TableCell>
+                  <TableCell>Closed</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Wednesday</strong>
+                  </TableCell>
+                  <TableCell>Closed</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Thursday</strong>
+                  </TableCell>
+                  <TableCell>4:00 PM–9:00 PM</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Friday</strong>
+                  </TableCell>
+                  <TableCell>4:00 PM–9:00 PM</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">
+                    <strong>Saturday</strong>
+                  </TableCell>
+                  <TableCell>11:00 AM–8:00 PM</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+            <Text gridArea="main"></Text>
+          </Box>
+          <Box pad="small">
+            <Anchor href="menu" color="neutral-2">
+              <Box pad="xsmall" height="small" width="small" border={true}>
+                <Heading level="3" alignSelf="center">
+                  Menu
+                </Heading>
+                <Image fit="cover" src="menu.png" />
+              </Box>
+            </Anchor>
+          </Box>
+          <Box pad="small">
+            <Image fill="horizontal" src="frites.jpg" />
+          </Box>
+        </Box>
       </Main>
-
-      <Footer background="brand" pad="medium" fill="horizontal">
-        <Text>&#169; Sister Helen {new Date().getFullYear()}</Text>
-        <Anchor
-          label="Contact"
-          href="mailto:hello@sisterhelenphx.com"
-          color="accent-4"
-        />
-      </Footer>
-    </Grommet>
+    </div>
   );
 }
