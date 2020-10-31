1e485643ca773bdd482626a95b8e1f6711cb6c4f
Jonathan Rusk <jrusk@chromis.com>
2020-10-31 14:13:33 -0700

    add menu and hours, tweak colors and layout

---
+++ pages/menu.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { Anchor, Box, Heading, Image, Main, Text } from "grommet";
+
+export default function Menu() {
+  return (
+    <div>
+      <Head>
+        <title>Sister Helen | Menu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Main pad="large">
+        <Box direction="row" pad="medium">
+          <Box pad="small">
+            <Anchor href="/" label="Home" color="neutral-2" />
+          </Box>
+          <Box pad="small">>></Box>
+          <Box pad="small">
+            <Text size="medium" weight="bold" color="brand">
+              Sister Helen Menu
+            </Text>
+          </Box>
+          <Box pad="small">
+            <Anchor
+              href="menu.pdf"
+              label="Menu PDF version"
+              color="neutral-2"
+            />
+          </Box>
+        </Box>
+        <Image fill="horizontal" fit="contain" src="/menu.png" />
+      </Main>
+    </div>
+  );
+}
